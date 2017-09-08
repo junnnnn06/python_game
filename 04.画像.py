b5fb0159428775b939bf7f1b345902e1567c4dfb
junnnnn06 <junnnnn06@icloud.com>
2017-09-08 19:47:06 +0900

    画像表示

---
+++ 04.画像.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+from pygame.locals import QUIT,Rect
+
+pygame.init()
+SURFACE = pygame.display.set_mode((800,400))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    logo = pygame.image.load('/Users/JUN/python.jpg')
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        SURFACE.fill((225,225,225))
+        SURFACE.blit(logo, (0,0))
+        SURFACE.blit(logo, (250,50),Rect(50,100,100,100))
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
